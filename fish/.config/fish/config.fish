7348c14ffe9b92a945a0808870bc2d09bd06e141
Nadim Tawileh <nadim@tawileh.com>
2022-04-19 09:18:31 -0700

    added starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,3 +27,6 @@ set -gx GOBIN $GOPATH/bin
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path.fish.inc"; end
+
+starship init fish | source
+
